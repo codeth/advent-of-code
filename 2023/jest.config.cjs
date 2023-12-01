410094613f6f7f78ef0176382aa4c32bd01ee8ea
Tom Hills <tom@codeth.co.uk>
2023-12-01 23:14:48 +0000

    Config adjustments

--- 2023/jest.config.cjs
+++ 2023/jest.config.cjs
@@ -3,5 +3,8 @@ module.exports = {
   preset: 'ts-jest/presets/default-esm',
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^(\\.{1,2}/.*)\\.ts$': '$1',
   },
+  verbose: process.env.TEST_DEBUG === '1',
+  silent: process.env.TEST_DEBUG !== '1',
 };

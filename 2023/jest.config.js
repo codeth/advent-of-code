1173a81fea97ec92d7d8edfcf80e090b2d86dca8
Tom Hills <tom@codeth.co.uk>
2023-12-01 21:06:24 +0000

    Basic typescript project init for 2023

---
+++ 2023/jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};

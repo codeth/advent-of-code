0638a4fb6c706989f9a4a4a1e3e41aa74e502294
Tom Hills <tom@codeth.co.uk>
2023-12-09 21:34:46 +0000

    Puzzle 8 Part 2

--- 2023/puzzles/08/solver.test.ts
+++ 2023/puzzles/08/solver.test.ts
@@ -1,17 +1,37 @@
-import { end, parseData, processExampleInput, start, walk } from "./solver.js";
+import {
+  end,
+  findOptimalGhostWalkSteps,
+  parseData,
+  processExample2Input,
+  processExampleInput,
+  start,
+  walk,
+} from "./solver.js";
 
 test("returns the expected values from the example input", async () => {
   const data = await parseData(processExampleInput);
 
-  const { totalSteps } = walk(start, end, data);
+  const walked = walk(start, end, data);
 
-  expect(totalSteps).toEqual(2);
+  expect(walked.totalSteps).toEqual(2);
+});
+
+test("returns the expected values from the part 2 example input", async () => {
+  const data = await parseData(processExample2Input);
+
+  const { optimalSteps } = findOptimalGhostWalkSteps("A", "Z", data);
+
+  expect(optimalSteps).toEqual(6);
 });
 
 test("returns the correct sums from the input data", async () => {
   const data = await parseData();
 
-  const { totalSteps } = walk(start, end, data);
+  // Part 1
+  const walked = walk(start, end, data);
+  expect(walked.totalSteps).toEqual(19637);
 
-  expect(totalSteps).toEqual(19637);
+  // Part 2
+  const { optimalSteps } = findOptimalGhostWalkSteps("A", "Z", data);
+  expect(optimalSteps).toEqual(8811050362409);
 });
--- 2023/puzzles/08/solver.ts
+++ 2023/puzzles/08/solver.ts
@@ -8,6 +8,10 @@ const __dirname = path.dirname(__filename);
 
 export const processInput = makeInputProcessor(__dirname);
 export const processExampleInput = makeInputProcessor(__dirname, "example.txt");
+export const processExample2Input = makeInputProcessor(
+  __dirname,
+  "example-2.txt",
+);
 
 export const start = "AAA";
 export const end = "ZZZ";
@@ -25,6 +29,20 @@ export interface DesertPathData {
   directions: Directions;
   nodes: DesertNode[];
 }
+
+const move = (
+  currentNode: DesertNode,
+  pathIndex: number,
+  { directions, nodes }: DesertPathData,
+) => {
+  const nextLocation = currentNode.waypoint[directions[pathIndex]!];
+  const nextNode = nodes.find(({ location }) => location === nextLocation);
+
+  if (!nextNode)
+    throw new Error(`Unable to find next location: ${nextLocation}`);
+
+  return nextNode;
+};
 
 export const walk = (
   from: string,
@@ -44,13 +62,7 @@ export const walk = (
     // Repeat directions when they have been exhausted but destination not reached
     if (pathIndex === directions.length) pathIndex = 0;
 
-    const nextLocation = currentNode.waypoint[directions[pathIndex]!];
-    const nextNode = nodes.find(({ location }) => location === nextLocation);
-
-    if (!nextNode)
-      throw new Error(`Unable to find next location: ${nextLocation}`);
-
-    currentNode = nextNode;
+    currentNode = move(currentNode, pathIndex, { directions, nodes });
     pathIndex++;
     totalSteps++;
   }
@@ -60,8 +72,97 @@ export const walk = (
   };
 };
 
+export interface GhostWalkReturn {
+  pathCycles: number;
+  totalSteps: number;
+  startNode: DesertNode;
+  endNode: DesertNode;
+  pathIndex: number;
+  currentCycleStartNode: DesertNode;
+}
+
+export const ghostWalk = (
+  startNode: DesertNode,
+  endFlag: string,
+  { directions, nodes }: DesertPathData,
+): GhostWalkReturn => {
+  const isValidEndNode = (node: DesertNode) => node.location.endsWith(endFlag);
+
+  let pathIndex = 0;
+  let pathCycles = 0;
+  let totalSteps = 0;
+  let currentNode = startNode;
+  let currentCycleStartNode = startNode;
+
+  while (!isValidEndNode(currentNode)) {
+    // Repeat directions when they have been exhausted but destination not reached
+    if (pathIndex === directions.length) {
+      pathIndex = 0;
+      pathCycles++;
+      currentCycleStartNode = currentNode;
+    }
+
+    currentNode = move(currentNode, pathIndex, { directions, nodes });
+    pathIndex++;
+    totalSteps++;
+  }
+
+  return {
+    totalSteps,
+    pathCycles,
+    startNode,
+    pathIndex,
+    currentCycleStartNode,
+    endNode: currentNode,
+  };
+};
+
+const gcd = (a: number, b: number): number => {
+  return b == 0 ? a : gcd(b, a % b);
+};
+
+const lcm = (a: number, b: number) => (a * b) / gcd(a, b);
+
+const lcmFromArray = (...numbers: number[]) => {
+  numbers.sort((a, b) => a - b);
+
+  const initial = numbers.splice(0, 2) as [number, number];
+
+  return numbers.reduce(
+    (result, num) => {
+      return lcm(result, num);
+    },
+    lcm(...initial),
+  );
+};
+
+export const findOptimalGhostWalkSteps = (
+  startFlag: string,
+  endFlag: string,
+  { directions, nodes }: DesertPathData,
+) => {
+  const startNodes = nodes.filter(({ location }) =>
+    location.endsWith(startFlag),
+  );
+
+  if (!startNodes) throw new Error("Locations not found");
+
+  const results = startNodes.map((startNode) =>
+    ghostWalk(startNode, endFlag, { directions, nodes }),
+  );
+
+  const optimalSteps = lcmFromArray(
+    ...results.map(({ totalSteps }) => totalSteps),
+  );
+
+  return {
+    results,
+    optimalSteps,
+  };
+};
+
 export const toDesertNode = (line: string): DesertNode => {
-  const [location, left, right] = [...line.matchAll(/[A-Z]{3}/g)];
+  const [location, left, right] = [...line.matchAll(/[A-Z0-9]{3}/g)];
 
   if (!location || !left || !right) throw new Error("Invalid node data");
 
